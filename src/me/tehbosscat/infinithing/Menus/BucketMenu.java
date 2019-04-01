107b5863b86a158c6a494d1aa5d0a5f1976a4546
Max Barker <max@the-barkers.com>
2019-04-01 19:09:56 +0100

    Infini-Bucket Menu
    When an empty IB is clicked, a menu is opened to let the user choose which bucket they want. To change bucket type from one to another, the user needs to throw the bucket on the ground and return to an empty bucket.

--- src/me/tehbosscat/infinithing/Menus/BucketMenu.java
+++ src/me/tehbosscat/infinithing/Menus/BucketMenu.java
@@ -1,16 +1,93 @@
 package me.tehbosscat.infinithing.Menus;
 
+import me.tehbosscat.infinithing.Items.InfiniBucket;
+import me.tehbosscat.infinithing.Items.InfiniBucketLava;
+import me.tehbosscat.infinithing.Items.InfiniBucketWater;
 import me.tehbosscat.infinithing.Main;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class BucketMenu implements Listener {
+    private final static String NAME = "Select an " + ChatColor.GOLD + "Infini" + ChatColor.WHITE + "Bucket";
+    private final static int inventSize = 9;
+
+    private final static String BUTTON_LORE = "Click to select this bucket type.";
 
     public void newBucketMenu(Player player){
-        Inventory i = Main.server.createInventory(null, 9, ChatColor.GOLD + "Infini" + ChatColor.WHITE + "Bucket Menu");
-        player.openInventory(i);
+        Inventory menu = Main.server.createInventory(null, inventSize, NAME);
+
+        ItemStack empty = AddMeta(new ItemStack(Material.STAINED_GLASS_PANE,1, (byte) 15));
+        ItemStack lavaButton = InfiniBucket.AddMeta(new ItemStack(Material.LAVA_BUCKET, 1), InfiniBucketLava.NAME, InfiniBucketLava.getLoreString(), BUTTON_LORE);
+        ItemStack waterButton = InfiniBucket.AddMeta(new ItemStack(Material.WATER_BUCKET, 1), InfiniBucketWater.NAME, InfiniBucketWater.getLoreString(), BUTTON_LORE);
+
+        for (int i = 0; i < inventSize; i++) {
+            switch (i) {
+                case 3:
+                    menu.setItem(i, lavaButton);
+                    break;
+                case 5:
+                    menu.setItem(i, waterButton);
+                    break;
+                default:
+                    menu.setItem(i, empty);
+                    break;
+            }
+        }
+
+        player.openInventory(menu);
+    }
+
+    private static ItemStack AddMeta(ItemStack item){
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName(" ");
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event){
+        Inventory inventory = event.getInventory();
+
+        if(inventory == null) return;
+        if(!inventory.getName().equals(NAME)) return;
+
+        event.setCancelled(true);
+        Player player = (Player) event.getWhoClicked();
+        ItemStack item = event.getCurrentItem();
+
+        if(item == null || !item.hasItemMeta()) return;
+
+        ItemStack emptyBucket = Main.empty.CreateInstance();
+
+        if(item.getItemMeta().getDisplayName().equals(InfiniBucketLava.NAME)){
+            ReplaceBucket(player, emptyBucket, Main.lava.CreateInstance());
+            player.closeInventory();
+        }else if (item.getItemMeta().getDisplayName().equals(InfiniBucketWater.NAME)){
+            ReplaceBucket(player, emptyBucket, Main.water.CreateInstance());
+            player.closeInventory();
+        }
+    }
+
+    public static void ReplaceBucket(Player player, ItemStack oldBucket, ItemStack newBucket){
+        String bucketName = oldBucket.getItemMeta().getDisplayName();
+        Inventory inventory = player.getInventory();
+
+        for (int i = 0; i < inventory.getSize(); i++) {
+            if (inventory.getContents()[i].getItemMeta().getDisplayName().equals(bucketName)){
+                inventory.setItem(i , newBucket);
+                break;
+            }
+        }
     }
 }
