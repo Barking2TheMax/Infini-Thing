27e72cb086436693ece8c26f84bd5f7b262aa549
Max Barker <max@the-barkers.com>
2019-06-24 21:06:11 +0100

    Removed BucketButton redundancy.
    Refactored LavaButtonBehvaiour and WaterButtonBehaviour to reduce code redundancy and created a parent class called BucketButtonBehaviour that handles the main interaction.

---
+++ src/me/tehbosscat/infinithing/Menus/BucketMenuBehaviours/BucketButtonBehaviour.java
@@ -0,0 +1,33 @@
+package me.tehbosscat.infinithing.Menus.BucketMenuBehaviours;
+
+import me.tehbosscat.infinithing.Items.InfiniItemFactory;
+import me.tehbosscat.infinithing.Menus.BaseMenu.I_OnClickBehaviour;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class BucketButtonBehaviour implements I_OnClickBehaviour {
+    protected InfiniItemFactory f = InfiniItemFactory.GetInstance();
+    protected ItemStack bucket = f.CreateItem("lava");
+
+    public boolean Clicked(Player player) {
+        Inventory inventory = player.getInventory();
+
+        ItemStack emptyBucket = f.CreateItem("empty");
+
+        for (int i = 0; i < inventory.getSize(); i++) {
+            ItemStack itemStack = inventory.getItem(i);
+
+            if (itemStack == null) continue;
+
+            if (itemStack.isSimilar(emptyBucket)) {
+                itemStack.setAmount(itemStack.getAmount() - 1);
+                inventory.setItem(i, itemStack);
+                inventory.addItem(bucket);
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
--- src/me/tehbosscat/infinithing/Menus/BucketMenuBehaviours/WaterButtonBehaviour.java
+++ src/me/tehbosscat/infinithing/Menus/BucketMenuBehaviours/WaterButtonBehaviour.java
@@ -3,31 +3,12 @@ package me.tehbosscat.infinithing.Menus.BucketMenuBehaviours;
 import me.tehbosscat.infinithing.Items.InfiniItemFactory;
 import me.tehbosscat.infinithing.Menus.BaseMenu.I_OnClickBehaviour;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
-public class WaterButtonBehaviour implements I_OnClickBehaviour {
+public class WaterButtonBehaviour extends BucketButtonBehaviour implements I_OnClickBehaviour {
 
     public boolean Clicked(Player player) {
-        InfiniItemFactory f = InfiniItemFactory.GetInstance();
-        Inventory inventory = player.getInventory();
-
-        ItemStack emptyBucket = f.CreateItem("empty");
-        ItemStack waterBucket = f.CreateItem("water");
-
-        for (int i = 0; i < inventory.getSize(); i++) {
-            ItemStack itemStack = inventory.getItem(i);
-
-            if (itemStack == null) continue;
-
-            if (itemStack.isSimilar(emptyBucket)){
-                itemStack.setAmount(itemStack.getAmount() - 1);
-                inventory.setItem(i , itemStack);
-                inventory.addItem(waterBucket);
-                return true;
-            }
-        }
-
-        return false;
+        super.f = InfiniItemFactory.GetInstance();
+        super.bucket = f.CreateItem("water");
+        return super.Clicked(player);
     }
 }
